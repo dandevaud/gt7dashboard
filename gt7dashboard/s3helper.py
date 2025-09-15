60270620226af542b11ceb23ab9a1a03cef4776b
Daniel Devaud <dan@ddev.ch>
2025-09-15 15:27:54 +0200

    Enhance S3 helper functions: add bucket_name parameter to upload_json_object, implement list_objects and get_object functions for improved S3 interaction

--- gt7dashboard/s3helper.py
+++ gt7dashboard/s3helper.py
@@ -5,12 +5,13 @@ from minio import Minio
 from minio.error import S3Error
 
 
-def upload_json_object(obj, object_name):
+def upload_json_object(obj, object_name, bucket_name: str = None):
     # Read configuration from environment variables
     endpoint = os.environ.get("S3_ENDPOINT")
     access_key = os.environ.get("S3_ACCESS_KEY")
     secret_key = os.environ.get("S3_SECRET_KEY")
-    bucket_name = os.environ.get("S3_BUCKET_NAME")
+    if bucket_name is None:
+        bucket_name = os.environ.get("S3_BUCKET_NAME")
     secure = os.environ.get("S3_SECURE", "true").lower() == "true"
 
     if not all([endpoint, access_key, secret_key, bucket_name]):
@@ -40,5 +41,57 @@ def upload_json_object(obj, object_name):
     except S3Error as err:
         print(f"Error uploading JSON object: {err}")
 
-# Example usage:
-# upload_file("localfile.txt")
+def list_objects(bucket_name: str = None ):
+    endpoint = os.environ.get("S3_ENDPOINT")
+    access_key = os.environ.get("S3_ACCESS_KEY")
+    secret_key = os.environ.get("S3_SECRET_KEY")
+    if bucket_name is None:
+        bucket_name = os.environ.get("S3_BUCKET_NAME")
+    secure = os.environ.get("S3_SECURE", "true").lower() == "true"
+
+    if not all([endpoint, access_key, secret_key, bucket_name]):
+        raise ValueError("Missing one or more required S3 environment variables.")
+
+    client = Minio(
+        endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=secure
+    )
+
+    try:
+        objects = client.list_objects(bucket_name)
+        return [obj.object_name for obj in objects]
+    except S3Error as err:
+        print(f"Error listing objects: {err}")
+        return []
+
+def get_object(object_name, bucket_name: str = None):
+    endpoint = os.environ.get("S3_ENDPOINT")
+    access_key = os.environ.get("S3_ACCESS_KEY")
+    secret_key = os.environ.get("S3_SECRET_KEY")
+    if bucket_name is None:
+        bucket_name = os.environ.get("S3_BUCKET_NAME")
+    secure = os.environ.get("S3_SECURE", "true").lower() == "true"
+
+    if not all([endpoint, access_key, secret_key, bucket_name]):
+        raise ValueError("Missing one or more required S3 environment variables.")
+
+    client = Minio(
+        endpoint,
+        access_key=access_key,
+        secret_key=secret_key,
+        secure=secure
+    )
+
+    try:
+        response = client.get_object(bucket_name, object_name)
+        data = response.read()
+        response.close()
+        response.release_conn()
+        return pickle.loads(data)
+    except S3Error as err:
+        print(f"Error retrieving object '{object_name}': {err}")
+        return None
+    
+    
